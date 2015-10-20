10cdc76e236f05e07c3705412f765000a69ad906
Lakmal Buddika Meegahapola <lakmalbuddikalucky@gmail.com>
2015-10-20 11:33:39 +0530

    Obstacle Location Detection
    This contains the method which was written to parse the server message
    mentioning the locations of the obstacles.

--- MyTankClient/MyTankClient/MyClient.cs
+++ MyTankClient/MyTankClient/MyClient.cs
@@ -26,9 +26,19 @@ namespace MyTankClient
 
         private Thread thread;
 
-        //to keep the player details
+        //to keep the initial player details
         private int playerLocationX, playerLocationY;
         private string playerName,direction;
+        
+        //initial locations of bricks,stones & water
+        private int[,] bricks,stones,water;
+
+        //details of each players and damage levels
+        private string[] player1, player2, player3, player4, player5,p1damage,p2damage,p3damage,p4damage,p5damage;
+        
+        //coins and life pakc availability
+        private string[][] coins,lifepacks;
+
 
         public MyClient() {
             thread = new Thread(new ThreadStart(recieveFromServer));
@@ -129,7 +139,54 @@ namespace MyTankClient
 
         private void decodeObstacleLocations(string data)
         {
-            throw new NotImplementedException();
+            string[] lines = Regex.Split(data, ":#");
+            string bricksStr = lines[2];
+            string stoneStr = lines[3];
+            string waterStr = lines[4];
+
+            string[] brickStrCordinates = Regex.Split(bricksStr, ";,");
+            string[] stoneStrCordinates = Regex.Split(bricksStr, ";,");
+            string[] waterStrCordinates = Regex.Split(bricksStr, ";,");
+
+            bricks = new int[2, (brickStrCordinates.Length / 2)];
+            stones = new int[2, (stoneStrCordinates.Length / 2)];
+            water = new int[2, (waterStrCordinates.Length / 2)];
+            
+            for (int x = 0; x < brickStrCordinates.Length; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    bricks[0, x/2] = Int32.Parse(brickStrCordinates[x]);
+                }
+                else
+                {
+                    bricks[1, x / 2] = Int32.Parse(brickStrCordinates[x]);
+                }
+            }
+
+            for (int x = 0; x < stoneStrCordinates.Length; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    stones[0, x / 2] = Int32.Parse(stoneStrCordinates[x]);
+                }
+                else
+                {
+                    stones[1, x / 2] = Int32.Parse(stoneStrCordinates[x]);
+                }
+            }
+
+            for (int x = 0; x < waterStrCordinates.Length; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    water[0, x / 2] = Int32.Parse(waterStrCordinates[x]);
+                }
+                else
+                {
+                    water[1, x / 2] = Int32.Parse(waterStrCordinates[x]);
+                }
+            }
         }
 
         private void decodeInitialPlayerLocations(string data)
